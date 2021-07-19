d591bb897dd30c7b78de174f2578bff917cfe5f0
Avinash Singh <61057666+avinash201199@users.noreply.github.com>
2021-07-19 20:26:02 +0530

    Create 235. Lowest Common Ancestor of a Binary Search Tree.java

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p, q);
+        }
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right,p, q);
+        }
+        
+        return root ==p?p:root==q?q:root;
+        
+    }
+}
