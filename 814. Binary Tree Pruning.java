9f3ad2c05267e217fe64a7138da780e848b7aed5
Avinash Singh <61057666+avinash201199@users.noreply.github.com>
2021-07-23 17:39:04 +0530

    Create 814. Binary Tree Pruning.java

---
+++ 814. Binary Tree Pruning.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root==null) return null;
+        root.left=pruneTree(root.left);
+        root.right=pruneTree(root.right);
+        if(root.left!=null || root.right!=null || root.val==1)
+            return root;
+        return null;
+}
+}
